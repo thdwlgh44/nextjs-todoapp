4f5e969c2dea871b3fc6cf21528a828a12bf1a9b
jihosong <thdwlgh44@naver.com>
2024-04-04 19:37:36 +0900

    content-type check 코드 추가

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -5,8 +5,12 @@ async function fetchTodosApiCall() {
 	console.log("fetchTodosApiCall called");
 	const res = await fetch(`${process.env.BASE_URL}/api/todos/`, {cache: 'no-store'})
  
-	const contentTypeCheck = res.headers.get('Content-Type')
-	console.log("fetchTodosApiCall / contentTypeCheck", contentTypeCheck);
+	const contentTypeHeaderValue = res.headers.get('Content-Type')
+
+	if (contentTypeHeaderValue?.includes("text/html")) {
+		console.log("fetchTodosApiCall / contentTypeHeaderValue", contentTypeHeaderValue);
+		return null;
+	}
 
 	return res.json();
 }
@@ -15,10 +19,12 @@ export default async function TodosPage() {
 
 	const response = await fetchTodosApiCall();
 
+	const fetchedTodos = response?.data ?? [];
+
 	return (
 		<div className="flex flex-col space-y-12">
 			<h1 className={title()}>Todos</h1>
-			<TodosTable todos={response.data ?? []}/>
+			<TodosTable todos={fetchedTodos} />
 			{/* <TodosTable todos={[]}/> */}
 		</div>
 	);
